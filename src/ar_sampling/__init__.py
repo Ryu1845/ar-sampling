f829c2c4782a125b15f1f6cf75aca4e9a575ec3a
Ryu1845 <ryu@tpgjbo.xyz>
2023-06-06 15:35:58 +0200

    add greedy search stub

--- src/ar_sampling/__init__.py
+++ src/ar_sampling/__init__.py
@@ -1,2 +1,9 @@
-def hello():
-    return "Hello from ar-sampling!"
+from sys import maxsize
+from typing import TypeVar
+
+from .model_proto import Model
+
+Tensor = TypeVar("Tensor") # TODO use jaxtyping
+
+def greedy_search(model: Model, input_ids: Tensor,*, eos_token_id: int, max_length: int = maxsize):
+    raise NotImplemenentedError()
